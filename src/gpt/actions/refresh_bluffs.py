81f0dbc688ed79d920c0d2f8e5c12f55d1a92357
Sam Deans <samuel.deans@jic.ac.uk>
2024-04-14 22:36:29 +0100

    feat: split out get_matches

--- src/gpt/actions/refresh_bluffs.py
+++ src/gpt/actions/refresh_bluffs.py
@@ -2,6 +2,8 @@ import asyncio
 import datetime as dt
 import logging
 import openai
+import re
+from typing import List
 from beanie.operators import In, And
 from db import create_client, start_beanie_session, get_db
 from db.football import Match, Transfer, TransferType
@@ -10,6 +12,7 @@ import gpt.prompts.bluffball as bluffball
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+DEFAULT_COUNTRIES = ("England", "Scotland", "Wales",)
 
 def _get_gpt_response(content: str, base_prompt: str = bluffball.prompt):
     logger.debug(f"Getting GPT response for {content}")
@@ -39,7 +42,34 @@ def generate_from_prompt(
     return bluff
 
 
-async def refresh_bluffs():
+async def get_matches(n=100, countries = DEFAULT_COUNTRIES, skip_youth: bool = True, bluffed_already: bool = False) -> List[Match]:
+    await start_beanie_session()
+    all_unbluffed_matches: List[Match] = await (
+        Match
+        .find(
+            And(
+                Match.bluffed == bluffed_already, 
+                In(Match.country, countries)
+            )
+        )
+        .to_list())
+    logger.info(f"Got {len(all_unbluffed_matches)} matches from DB: countries={countries}, bluffed_already={bluffed_already}")
+
+    if skip_youth:
+        def has_youth(m: Match) -> bool:
+            youth_regex = r"U[0-9]"
+            has_comp = re.search(youth_regex, m.competition)
+            has_home = re.search(youth_regex, m.homeTeam)
+            has_away = re.search(youth_regex, m.awayTeam)
+            return bool(has_comp or has_home or has_away)
+        
+        all_unbluffed_matches = [m for m in all_unbluffed_matches if not has_youth(m)]
+        logger.info(f"Got {len(all_unbluffed_matches)} matches after filtering out youth matches")
+    
+    return all_unbluffed_matches[:n]
+
+
+async def refresh_bluffs(n=100, countries = DEFAULT_COUNTRIES, skip_youth: bool = True):
     logger.debug("Refreshing bluffs")
     await start_beanie_session()
     transfers = await Transfer.find(Transfer.bluffed == False).to_list(length=10)
@@ -51,8 +81,7 @@ async def refresh_bluffs():
         bluff = generate_from_prompt(content=transfer_str)
         await Bluff.insert(bluff)
         await Transfer.set(t, {"bluffed": True})
-    matches = await Match.find(And(Match.bluffed == False, In(Match.country, ("England", "Scotland", "Wales",)))).to_list(length=100)
-    logger.info(f"Got {len(matches)} matches")
+    matches = await get_matches(n=n, countries=countries, skip_youth=skip_youth, bluffed_already=False)
     for m in matches:
         match_str = m.json()
         bluff = generate_from_prompt(content=match_str)
@@ -67,3 +96,4 @@ if __name__ == "__main__":
     # x = generate_from_prompt(content=content)
     # b =1
     asyncio.run(refresh_bluffs())
+    # asyncio.run(get_matches(bluffed_already=True))
